d9131989bf6eab07ef0c9405e61bf29c898713cb
thePHProgrammer <101506934+thePHProgrammer@users.noreply.github.com>
2023-09-12 14:40:50 +0800

    Update JSMETACRAFTER.js

--- JSMETACRAFTER.js
+++ JSMETACRAFTER.js
@@ -7,36 +7,45 @@ Assessment Requirements
 3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
 4. For good measure, getTotalSupply() should return the number of NFT's you have created
 */
-
 // create a variable to hold your NFT's
-const NFTs = []
+const NFTs = [];
+
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT (_name, _nickname, _favoriteArtist, _bling) {
+function mintNFT(_name, _nickname, _favoriteArtist, _bling) {
     const NFT = {
         "name": _name,
         "nickname": _nickname,
         "favoriteArtist": _favoriteArtist,
         "bling": _bling
     }
-    NFTs.push(NFT)
+    NFTs.push(NFT);
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
-function listNFTs () {
-    for(let i = 0; i<NFTs.length; i++){
-        console.log(NFTs[i])
+function listNFTs() {
+    console.log("List of NFTs:");
+    for (let i = 0; i < NFTs.length; i++) {
+        console.log(`NFT ${i + 1}:`);
+        console.log(`Name: ${NFTs[i].name}`);
+        console.log(`Nickname: ${NFTs[i].nickname}`);
+        console.log(`Favorite Artist: ${NFTs[i].favoriteArtist}`);
+        console.log(`Bling: ${NFTs[i].bling}`);
+        console.log("--------------------");
     }
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-    console.log(NFTs.length);
+    console.log(`Total NFTs Minted: ${NFTs.length}`);
 }
 
 // call your functions below this line
-mintNFT("Noriel", "Nori", "Swift", "Necklace")
+mintNFT("Noriel", "Nori", "Swift", "Necklace");
+mintNFT("Alice", "Alicia", "Picasso", "Crown");
+mintNFT("Bob", "Bobby", "Van Gogh", "Ring");
+
 listNFTs();
 getTotalSupply();
